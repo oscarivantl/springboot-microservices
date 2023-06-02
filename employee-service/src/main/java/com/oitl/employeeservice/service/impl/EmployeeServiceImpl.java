608b28fb435b95c8822e15587ca035dccfc286a8
Oscar Ivan Torres Lopez <oscar.torres@unipamplona.edu.co>
2023-06-02 18:57:16 -0500

    Microservices Communication using WebClient

--- employee-service/src/main/java/com/oitl/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/oitl/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -17,7 +18,8 @@ public class EmployeeServiceImpl  implements EmployeeService {
 
     EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+    //private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -46,10 +48,16 @@ public class EmployeeServiceImpl  implements EmployeeService {
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> responseEntity =  restTemplate.getForEntity(
+        /*ResponseEntity<DepartmentDto> responseEntity =  restTemplate.getForEntity(
                 "http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = responseEntity.getBody();*/
+
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),

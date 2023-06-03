a655030a04523fa7c550a2798294e07422616143
Oscar Ivan Torres Lopez <oscar.torres@unipamplona.edu.co>
2023-06-02 20:16:11 -0500

    Create and Setup service-registry Spring boot project

---
+++ service-registry/src/test/java/com/oitl/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oitl.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
